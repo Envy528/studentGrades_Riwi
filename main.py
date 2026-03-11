f56febf2e2b9fd4ad31ca678567686ca87dfcf6a
Juanpoveda115 <juandavidpoa@gmail.com>
2026-03-11 10:22:40 -0500

    Feature 2: Subject and Grades Registration

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ studentsList = []
 option = None
 
 while option != 2:
-    option = input("\n1. Register a student\n2. Show registered students\n3. Leave\nChoose an option: ")
+    option = input("\n1. Register a student.\n2. Show registered students.\n3. Leave.\nChoose an option: ")
 
     if option == "1":
         try:
             studentsQuantity = int(input("\nHow many students are you going to register?\nNumber: "))
         
             for i in range(studentsQuantity):
-                studentFullName = input(f"\nEnter the {i+1} student's full name\nFull name: ")
+                subjectsAndGrades = []
+                studentFullName = input(f"\nEnter the {i+1} student's full name.\nFull name: ")
                 
-                studentsList.append(studentFullName)
+                subjectQuantity = int(input("\nHow many subjects are you going to register for this student?\nNumber: "))
+                
+                for i in range(subjectQuantity):
+                    subject = input(f"\nEnter the {i+1} subject for the student.\nSubject: ")
+                    grade = float(input(f"\nEnter the {subject} grade.\nGrade: "))
+
+                    subjectsAndGrades.append({
+                        "subject":subject,
+                        "grade":grade,
+                    })
+                
+                
+                studentsList.append({
+                    "studentName":studentFullName,
+                    "subjectsAndGrades":subjectsAndGrades
+                })
 
         except:
             print("\n----- ERROR -----\nEnter a valid number")
@@ -20,8 +36,10 @@ while option != 2:
     elif option == "2":
         print("\n----- Students -----")
         for i, student in enumerate(studentsList):
-            print(f"{i+1}. {student}")
+            print(f"{i+1}. {student['studentName']}\n")
+            
+            for i, studentSubjectAndGrade in enumerate(student["subjectsAndGrades"]):
+                print(f"{i+1}--Subject: {studentSubjectAndGrade['subject']}\n----Grade: {studentSubjectAndGrade['grade']}\n")
         print("-"*20)
-    
     else:
         print("Choose a valid option.")
